5278308280077fd8e6ac63fc28f2e2568c11d0f6
Chris Soncrant <christopher.soncrant@gmail.com>
2022-10-25 16:49:51 -0700

    completed Icon Input challenge

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -9,23 +9,52 @@ import VisuallyHidden from '../VisuallyHidden';
 const IconInput = ({
   label,
   icon,
-  width = 150,
+  width,
   size,
-  placeholder,
+  placeholder
 }) => {
+
+  const SIZES = {
+    small: {
+      fontSize: '0.875rem',
+      width: '150px',
+      iconSize: 16,
+      height: '1.5rem',
+      border: '1px solid',
+    },
+    large: {
+      fontSize: '1.125rem',
+      width: '300px',
+      iconSize: 24,
+      height: '2.25rem',
+      border: '3px solid',
+    },
+  };
+
+  const currentSize = SIZES[size];
+
+  if(!currentSize) {
+    throw new Error(`Invalid size value: ${size}`);
+  }
   
-  return (
-    
-    
-    <Wrapper>
+  return ( 
+    <Wrapper
+      fontSize={currentSize.fontSize}
+    >
       <VisuallyHidden>{label}</VisuallyHidden>
 
-      <InputIcon id={icon} />
+      <IconWrapper
+        size={currentSize.iconSize}
+      >
+        <Icon id={icon} size={currentSize.iconSize} />
+      </IconWrapper>
 
       <TextInput 
         placeholder={placeholder}
-        size={size}
-        width={width + 'px'}
+        style={{
+          '--width': currentSize.width, '--height': currentSize.height, '--border': currentSize.border,
+          '--padding': currentSize.height,
+        }} 
       />
     </Wrapper>
   )
@@ -34,33 +63,39 @@ const IconInput = ({
 const Wrapper = styled.label`
   display: block;
   position: relative;
-`
+  color: ${COLORS.gray700};
+  font-size: ${props => props.fontSize};
+  
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
 
-const InputIcon = styled(Icon)`
+const IconWrapper = styled.div`
   position: absolute;
-  pointer-events: none;
-  height: ${24 / 16}rem;
   top: 0;
   bottom: 0;
-  margin: auto;
-`
+  margin: auto 0px;
+  height: ${props => props.size}px;
+`;
 
 const TextInput = styled.input`
-  height: ${24 / 16}rem;
-  width: ${props => {
-    console.log(props.width)
-    return props.width
-  }};
-  /* width: 400px; */
+  font: inherit;
+  height: var(--height);
+  width: var(--width);
   border: none;
-  border-bottom: 1px solid ${COLORS.black};
-  padding: 1rem 0rem 1rem 2rem;
+  border-bottom: var(--border);
+  padding-left: var(--padding);
+  font-weight: 700;
 
   &::placeholder {
-
+    color: ${COLORS.gray500};
+    font-weight: 400;
   }
-`
 
-
+  &:focus {
+    outline-offset: 2px;
+  }
+`;
 
 export default IconInput;
