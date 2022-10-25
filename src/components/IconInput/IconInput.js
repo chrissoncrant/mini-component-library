297355f439b90e29215a9cb1569c26f28f980510
Chris Soncrant <christopher.soncrant@gmail.com>
2022-10-24 20:25:42 -0700

    completed the selector challenge; started on the icon input challenge

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -9,11 +9,58 @@ import VisuallyHidden from '../VisuallyHidden';
 const IconInput = ({
   label,
   icon,
-  width = 250,
+  width = 150,
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  
+  return (
+    
+    
+    <Wrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+
+      <InputIcon id={icon} />
+
+      <TextInput 
+        placeholder={placeholder}
+        size={size}
+        width={width + 'px'}
+      />
+    </Wrapper>
+  )
 };
+
+const Wrapper = styled.label`
+  display: block;
+  position: relative;
+`
+
+const InputIcon = styled(Icon)`
+  position: absolute;
+  pointer-events: none;
+  height: ${24 / 16}rem;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+`
+
+const TextInput = styled.input`
+  height: ${24 / 16}rem;
+  width: ${props => {
+    console.log(props.width)
+    return props.width
+  }};
+  /* width: 400px; */
+  border: none;
+  border-bottom: 1px solid ${COLORS.black};
+  padding: 1rem 0rem 1rem 2rem;
+
+  &::placeholder {
+
+  }
+`
+
+
 
 export default IconInput;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -8,11 +8,78 @@ import { getDisplayedValue } from './Select.helpers';
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
+  console.log('test', children);
+
+
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <SelectElement value={value} onChange={onChange}>
+        {children}
+      </SelectElement>
+      <Presentation>
+        {displayedValue}
+      </Presentation>
+
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  font-size: 4rem;
+  width: max-content;
+`;
+
+const SelectElement = styled.select`
+  appearance: none;
+  -webkit-appearance: none;
+  cursor: pointer;
+  font: inherit;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  z-index: 1;
+`;
+
+const Presentation = styled.div`
+  position: relative;
+  background-color: ${COLORS.transparentGray15};
+  color: ${COLORS.gray700};
+  font-size: 1em;
+  padding: 0.75em 1em 0.65em;
+  padding-right: 4em;
+  border-radius: 0.3em;
+  display: flex;
+  align-items: center;
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 1em;
+    bottom: 0;
+    margin: auto;
+    box-sizing: border-box;
+    width: 0.5em;
+    height: 0.5em;
+    border-bottom: 0.2em solid;
+    border-right: 0.2em solid;
+    transform: rotate(45deg);
+  }
+
+  ${SelectElement}:hover + & {
+    background-color:rgb(176, 176, 176);
+    color: black;
+  }
+
+  ${SelectElement}:focus + & {
+    outline: 0.15em solid slateblue;   
+    background-color:rgb(176, 176, 176);
+    color: black;
+  }
+`;
 
 export default Select;
